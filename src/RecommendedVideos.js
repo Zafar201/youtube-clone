407f812486cc8feb77fbdbb4bc09895e009bf046
Zafar Irshad <zafarirshad201@gmail.com>
2020-10-01 14:58:17 +0530

    completed ready to deploy

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -8,14 +8,74 @@ function RecommendedVideos() {
         <div className="recommendedVideos">
             <h2>Recommended</h2>
             <div className="recommendedVideos__videos">
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
-                 <VideoCard/>
+                 <VideoCard
+                 title="Become a webDeveloper in 10 minutes | 2019/2020"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s100-c-k-c0xffffffff-no-rj-mo"
+                 channel="Sonny Sangha"
+                 image="https://i.ytimg.com/vi/UaB_FtsdlYk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBiSCrWOo4xSWC7PtRiqD5WKcGVpg"
+                 />
+                  <VideoCard
+                 title=" WHETHER WE LIKE IT OR NOT"
+                 views="2.3M Views"
+                 timestamp="10days ago"
+                 channelImage="https://yt3.ggpht.com/a/AATXAJxTcyC0gXq6OAi10gPDE51KiTDpzJvayEAEgd7oKA=s176-c-k-c0x00ffffff-no-rj-mo"
+                 channel="Sonny Sangha"
+                 image="https://i.ytimg.com/vi/5JGlr-FVKsk/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDpqEdKba2dusPvXa6kUZBc914M4Q"
+                 />
+                  <VideoCard
+                 title="5 Uses for cloud functions"
+                 views="2.3M Views"
+                 timestamp="1month ago"
+                 channelImage="https://yt3.ggpht.com/a-/AOh14Gg-7xgPV76_c0nFmsYJLGUTUkJCrPWcnyULag=s68-c-k-c0x00ffffff-no-rj-mo"
+                 channel="Sonny Sangha"
+                 image="https://i.ytimg.com/vi/77XmRDtOL7c/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDIiXYbD6TYhgtfWFv2qvFQ8WgQLA"
+                 />
+                  <VideoCard
+                 title="The Truth about OnePlus Nord!"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://yt3.ggpht.com/a-/AOh14Ghl8U3OeJOtu1LRmTNuW7Aiq-iKaU5U6gYWoA=s68-c-k-c0x00ffffff-no-rj-mo"
+                 channel="Sonny Sangha"
+                 image="https://i.ytimg.com/vi/Xzh8BdaaAvs/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBFVU97RnCsjtboTaR3TQzvoosgWg"
+                 />
+                  <VideoCard
+                 title="Elon Musk says Teslas are too expensive!"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
+                 channel="Sonny Sangha"
+                 image="https://i.ytimg.com/vi/ZMsDib3wfuY/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB9Wei5mn2iCrSIFFQCRfarGe6gBA"
+                 />
+                 <VideoCard
+                 title="Thiruvonam | Karikku | Comedy"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://yt3.ggpht.com/a/AATXAJwwMu7o77WhlpmiPF7yHJSfoA-y5fgbC8rGH-4sKg=s100-c-k-c0xffffffff-no-rj-mo"
+                 channel="Karikku"
+                 image="https://i.ytimg.com/vi/uV0F8LcU120/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCsxk8UWTQi44Am7BOSFRdT27diKA"
+                 />
+                  <VideoCard
+                 title="Dulquer Salmaan Teaches Popular Malayalam Phrases | Bollywood | Pinkvilla"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://yt3.ggpht.com/a-/AOh14GiGYHPHZVZDNK_Uq1go233feVA-U8E4D3GQkQ=s68-c-k-c0x00ffffff-no-rj-mo"
+                 channel="
+                 PINKVILLA
+                 "
+                 image="https://i.ytimg.com/an_webp/bHhM93bks6Y/mqdefault_6s.webp?du=3000&sqp=CPTL1fsF&rs=AOn4CLBzkpJnh77G9_Z7BI3JKPrwLFMYjA"
+                 />
+                 <VideoCard
+                 title="+2 Free Period | Comedy | Karikku"
+                 views="2.3M Views"
+                 timestamp="3days ago"
+                 channelImage="https://yt3.ggpht.com/a-/AOh14GjsTrvXDxa9iXGmCNOP7vJ2pj5zU3rGGP9aMA=s68-c-k-c0x00ffffff-no-rj-mo"
+                 channel="Karikku"
+                 image="https://i.ytimg.com/an_webp/kW0E_TcHgzg/mqdefault_6s_480x270.webp?du=3000&sqp=CLjH1fsF&rs=AOn4CLCNIk8-ogcA-NGkpoD_Y7MVu2cmJA"
+                 />
+                 
+                 
                  
             </div>
         </div>
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD-Qy5H2BJFco0zMtodrf7qOi6DItyRDso",
+    authDomain: "clone-e29b6.firebaseapp.com",
+    databaseURL: "https://clone-e29b6.firebaseio.com",
+    projectId: "clone-e29b6",
+    storageBucket: "clone-e29b6.appspot.com",
+    messagingSenderId: "403111465323",
+    appId: "1:403111465323:web:83e7443333638ff83a3c2e",
+    measurementId: "G-LLFBV0PNMW"
+  };
